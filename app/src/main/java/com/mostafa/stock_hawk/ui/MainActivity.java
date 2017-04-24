8f80cac619274b0450c1fc0f9e23c4289543edef
Mostafa-elgendy <mostafa.elgendy@fci.bu.edu.eg>
2017-04-24 02:42:07 +0200

    Solve Problems in adding search for any symbol.
    1- first when you Click on add floating button
    2- a dialog will appear to allow you to enter your symbol
    3- if the symbol is founded is will display agraph about it's status.
     4- if the symbol is not founded is will add it to the database.

--- app/src/main/java/com/mostafa/stock_hawk/ui/MainActivity.java
+++ app/src/main/java/com/mostafa/stock_hawk/ui/MainActivity.java
@@ -15,7 +15,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.InputType;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -107,20 +106,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                                             new String[]{input.toString().toUpperCase()}, null);
 
                                     if (c.getCount() > 0) {
-                                        Log.e(" Data : ", "data: " + c.getCount());
-                                        c.moveToNext();
+                                        String symbol = input.toString().toUpperCase();
+                                        Intent intent = new Intent(mContext, GraphActivity.class);
+                                        intent.putExtra("symbol", symbol);
+                                        startActivity(intent);
 
-                                        Log.e(" Data : ", "data: " + c.getString(1) + " " + c.getString(2));
-                                        //   String symbol = c.getString(c.getColumnIndex(QuoteColumns.SYMBOL));
-                                        // Intent intent = new Intent(mContext, GraphActivity.class);
-                                        //intent.putExtra("symbol", symbol);
-                                        //startActivity(intent);
-                                        /*Toast toast =
-                                                Toast.makeText(MainActivity.this, R.string.already_saved,
-                                                        Toast.LENGTH_LONG);
-                                        toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
-                                        toast.show();
-                                        return;*/
                                     } else {
                                         // Add the stock to DB
                                         mServiceIntent.putExtra("tag", "add");
